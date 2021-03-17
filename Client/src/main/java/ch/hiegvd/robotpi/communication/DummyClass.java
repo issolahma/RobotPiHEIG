5af2d49483e718d8153b337d11c921e2bb5c9ecf
David González León <david.gonzalezleon@heig-vd.ch>
2021-03-17 16:27:30 +0100

    added main project java and gitignore

---
+++ Client/src/main/java/ch/hiegvd/robotpi/communication/DummyClass.java
@@ -0,0 +1,8 @@
+package ch.hiegvd.robotpi.communication;
+/*
+ * @File DummyClass.java
+ * @Authors : David González León
+ * @Date 17 mars 2021
+ */
+//classe inutile, a supprimer quand tu commence a bosser c'est juste pour que le package apparaisse chez toi
+public class DummyClass {}
---
+++ Client/src/main/java/ch/hiegvd/robotpi/userinterface/Launcher.java
@@ -0,0 +1,12 @@
+/*
+ * @File Launcher.java
+ * @Authors : David González León
+ * @Date 17 mars 2021
+ */
+package ch.hiegvd.robotpi.userinterface;
+
+public class Launcher {
+   public static void main(String[] args) {
+      
+   }
+}
