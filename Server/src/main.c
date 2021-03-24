445a5f44302ffdc2276febe2c6938752c618b7b1
AnthoJack <to.jaccard@hotmail.ch>
2021-03-24 16:42:53 +0100

    Setup base skeleton for server app

---
+++ Server/src/main.c
@@ -0,0 +1,11 @@
+/*
+Taches:
+-Réceptionner les commandes du clients(server)
+-Gérer la connexion au client(server)
+-Piloter les moteurs(motor)
+-Gérer une connexion I2C:motor
+-Gestion et sauvegarde des paramètres:init
+-Interpretation des commandes du protocole:protocol
+-Lire le fichier de configuration:init
+-Gérer la caméra:camera
+*/
