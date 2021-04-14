5a26bf6a765d82ee96fd6da67f447166e7b52449
mink <vis0n@protonmail.com>
2021-04-14 16:37:15 +0200

    server skeleton

--- Server/src/server.c
+++ Server/src/server.c
@@ -1,7 +1,7 @@
 #include "include/server.h"
 
-int server(int argc, char *argv[]) {
-	int server_sockfd;
+int server() {
+	int server_sockfd = 0, client_sockfd = 0;
 	struct sockaddr_in server_addr;
 	struct sockaddr_in client_addr;
 
@@ -13,6 +13,21 @@ int server(int argc, char *argv[]) {
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	server_addr.sin_port = htons(9734);  // à redéfinir
+    
+    if (bind(server_sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+        fprintf(stderr, "Could not bind socket\n");
+    }
+    
+    listen(server_sockfd, 1);
+    
+    while(1) {
+        fprintf(stdout, "Waiting for clients...\n");
+        client_sockfd = accept(server_sockfd, (struct sockaddr*)NULL, NULL);
+        fprintf(stdout, "Connection established\n");
+        close(client_sockfd);
+        fprintf(stdout, "Bye\n");
+
+    }
 
 	return 0;
 }
