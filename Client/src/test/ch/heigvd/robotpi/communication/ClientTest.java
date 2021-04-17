b086d90f941e318f2e3106a3066ae15978c2957a
issolahma <22448610+issolahma@users.noreply.github.com>
2021-04-17 07:27:12 +0200

    create test folder, and clientTest file

---
+++ Client/src/test/ch/heigvd/robotpi/communication/ClientTest.java
@@ -0,0 +1,33 @@
+package ch.heigvd.robotpi.communication;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void connect() {
+    }
+
+    @Test
+    void isConnected() {
+    }
+
+    @Test
+    void disconnect() {
+    }
+
+    @Test
+    void ping() {
+    }
+
+    @Test
+    void goForward() {
+    }
+}
