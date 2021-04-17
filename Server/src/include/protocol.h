d30d0c33300a9a49c32bb6c0552cb216b805714f
mink <vis0n@protonmail.com>
2021-04-17 15:10:40 +0200

    add new commands

--- Server/src/include/protocol.h
+++ Server/src/include/protocol.h
@@ -14,12 +14,17 @@
 #define BKWD_OK 48
 #define ROTATE_LEFT_OK 49
 #define ROTATE_RIGHT_OK 50
-#define CMD_ERR 51
-#define PING 52
+#define FRONT_L_OK 51
+#define FRONT_R_OK 52
+#define BCK_L_OK 53
+#define BCK_R_OK 54
+#define CMD_ERR 55
+#define PING 56
 
 #define CMD_LEN 16
 
 void put_response(char *response, int response_code);
+
 int process_cmd(char *cmd, char *response);
 
 #endif
